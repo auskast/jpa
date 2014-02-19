1e6160ba6e1926009a6714f708f47744ab6bb815
Patrick Fitzgerald <auskast03@gmail.com>
2014-02-18 22:56:23 -0800

    Better setup of test dates so that running against mysql works too

--- src/test/java/info/dyndns/pfitz/jpaexamples/dao/BookDaoTest.java
+++ src/test/java/info/dyndns/pfitz/jpaexamples/dao/BookDaoTest.java
@@ -2,13 +2,14 @@ package info.dyndns.pfitz.jpaexamples.dao;
 
 import info.dyndns.pfitz.jpaexamples.model.Author;
 import info.dyndns.pfitz.jpaexamples.model.Book;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
 
 import javax.annotation.Resource;
-import java.util.Date;
 import java.util.List;
 
 import static com.google.common.collect.Lists.newArrayList;
@@ -20,9 +21,11 @@ public class BookDaoTest extends AbstractTestNGSpringContextTests {
     @Resource
     private BookDao bookDao;
 
+    private final DateTime dateTime = new DateTime(2014, 2, 27, 12, 34, 56, DateTimeZone.forID("America/Los_Angeles"));
+
     @Test
     public void testSave() throws Exception {
-        final Book book = bookDao.save(new Book("ABC-123", "My Book", "", 123, new Date(1234567890),
+        final Book book = bookDao.save(new Book("ABC-123", "My Book", "", 123, dateTime.toDate(),
                 new Author("Patrick", "Fitzgerald")));
         assertEquals(book.getAuthors().get(0).getId(), Integer.valueOf(1));
     }
@@ -38,7 +41,7 @@ public class BookDaoTest extends AbstractTestNGSpringContextTests {
     @Test(dependsOnMethods = "testSaveAgain")
     public void testFindByIsbn() throws Exception {
         final Book book = bookDao.findByIsbn("ABC-123");
-        assertEquals(book, new Book("ABC-123", "My Book", "", 456, new Date(1234567890),
+        assertEquals(book, new Book("ABC-123", "My Book", "", 456, dateTime.toDate(),
                 createAuthor(1, "Patrick", "Fitzgerald")));
     }
 
@@ -48,7 +51,7 @@ public class BookDaoTest extends AbstractTestNGSpringContextTests {
         bookDao.save(new Book("GHI-789", "My Book"));
         final List<Book> books = bookDao.findByTitle("My Book");
         assertEquals(books, newArrayList(
-                new Book("ABC-123", "My Book", "", 456, new Date(1234567890), createAuthor(1, "Patrick", "Fitzgerald")),
+                new Book("ABC-123", "My Book", "", 456, dateTime.toDate(), createAuthor(1, "Patrick", "Fitzgerald")),
                 new Book("GHI-789", "My Book")
         ));
     }
@@ -57,7 +60,7 @@ public class BookDaoTest extends AbstractTestNGSpringContextTests {
     public void testFindByAuthor() throws Exception {
         final List<Book> books = bookDao.findByAuthor(createAuthor(1, "Patrick", "Fitzgerald"));
         assertEquals(books, newArrayList(
-                new Book("ABC-123", "My Book", "", 456, new Date(1234567890), createAuthor(1, "Patrick", "Fitzgerald")),
+                new Book("ABC-123", "My Book", "", 456, dateTime.toDate(), createAuthor(1, "Patrick", "Fitzgerald")),
                 new Book("DEF-456", "Other Book", createAuthor(1, "Patrick", "Fitzgerald"))
         ));
     }
@@ -66,7 +69,7 @@ public class BookDaoTest extends AbstractTestNGSpringContextTests {
     public void testGetAll() throws Exception {
         final List<Book> books = bookDao.getAll();
         assertEquals(books, newArrayList(
-                new Book("ABC-123", "My Book", "", 456, new Date(1234567890), createAuthor(1, "Patrick", "Fitzgerald")),
+                new Book("ABC-123", "My Book", "", 456, dateTime.toDate(), createAuthor(1, "Patrick", "Fitzgerald")),
                 new Book("DEF-456", "Other Book", createAuthor(1, "Patrick", "Fitzgerald")),
                 new Book("GHI-789", "My Book")
         ));
